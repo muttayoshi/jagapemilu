97cb8b48a3734bc74f54a9e74121111d3886208b
Abdu <abdu@karadigital.co>
2024-02-15 17:49:43 +0700

    handle with try

--- pemilu/duanolduaempat/models.py
+++ pemilu/duanolduaempat/models.py
@@ -13,7 +13,7 @@ class Tps(TimeStampedModel):
     def __str__(self):
         from pemilu.locations.models import Kelurahan
         kelurahan = Kelurahan.objects.filter(code=self.name[:-3]).first()
-        return f"{kelurahan.kecamatan.kota.provinsi.name} - {kelurahan.kecamatan.kota.name} - {kelurahan.kecamatan.name} - {kelurahan.name}"
+        return f"{kelurahan.kecamatan.kota.provinsi.name} - {kelurahan.kecamatan.kota.name} - {kelurahan.kecamatan.name} - {kelurahan.name} | TPS: {self.name[-3:]}"
 
     class Meta:
         ordering = ('-created',)
--- pemilu/duanolduaempat/utils.py
+++ pemilu/duanolduaempat/utils.py
@@ -4,9 +4,10 @@ import json
 from pemilu.locations.models import Provinsi, Kota, Kecamatan, Kelurahan
 from pemilu.duanolduaempat.models import Tps, Chart, Image, Administration, AnomalyDetection
 
-def crawling_kpu_v1():
+def crawling_kpu_v1(province_code):
 
-    kelurahan = Kelurahan.objects.all()
+    # kelurahan = Kelurahan.objects.all()
+    kelurahan = Kelurahan.objects.filter(code__startswith=province_code).all()
 
     for k in kelurahan:
         kelurahan = k.code
@@ -56,30 +57,28 @@ def crawling_kpu_v1():
                             url=image
                         )
 
-                    data_administration = data['administrasi']
-
-                    for key, value in data_administration.items():
-                        if data_administration:
-                            Administration.objects.get_or_create(
-                                tps=datatps,
-                                suara_sah=value.get('suara_sah'),
-                                suara_total=value.get('suara_total'),
-                                pemilih_dpt_l=value.get('pemilih_dpt_l'),
-                                pemilih_dpt_p=value.get('pemilih_dpt_p'),
-                                pengguna_dpt_j=value.get('pengguna_dpt_j'),
-                                pengguna_dpt_l=value.get('pengguna_dpt_l'),
-                                pengguna_dpt_p=value.get('pengguna_dpt_p'),
-                                pengguna_dptb_j=value.get('pengguna_dptb_j'),
-                                pengguna_dptb_l=value.get('pengguna_dptb_l'),
-                                pengguna_dptb_p=value.get('pengguna_dptb_p'),
-                                suara_tidak_sah=value.get('suara_tidak_sah'),
-                                pengguna_total_j=value.get('pengguna_total_j'),
-                                pengguna_total_l=value.get('pengguna_total_l'),
-                                pengguna_total_p=value.get('pengguna_total_p'),
-                                pengguna_non_dpt_j=value.get('pengguna_non_dpt_j'),
-                                pengguna_non_dpt_l=value.get('pengguna_non_dpt_l'),
-                                pengguna_non_dpt_p=value.get('pengguna_non_dpt_p')
-                            )
+                    if data.get("administrasi"):
+                        value = data.get("administrasi")
+                        Administration.objects.get_or_create(
+                            tps=datatps,
+                            suara_sah=value.get('suara_sah'),
+                            suara_total=value.get('suara_total'),
+                            pemilih_dpt_l=value.get('pemilih_dpt_l'),
+                            pemilih_dpt_p=value.get('pemilih_dpt_p'),
+                            pengguna_dpt_j=value.get('pengguna_dpt_j'),
+                            pengguna_dpt_l=value.get('pengguna_dpt_l'),
+                            pengguna_dpt_p=value.get('pengguna_dpt_p'),
+                            pengguna_dptb_j=value.get('pengguna_dptb_j'),
+                            pengguna_dptb_l=value.get('pengguna_dptb_l'),
+                            pengguna_dptb_p=value.get('pengguna_dptb_p'),
+                            suara_tidak_sah=value.get('suara_tidak_sah'),
+                            pengguna_total_j=value.get('pengguna_total_j'),
+                            pengguna_total_l=value.get('pengguna_total_l'),
+                            pengguna_total_p=value.get('pengguna_total_p'),
+                            pengguna_non_dpt_j=value.get('pengguna_non_dpt_j'),
+                            pengguna_non_dpt_l=value.get('pengguna_non_dpt_l'),
+                            pengguna_non_dpt_p=value.get('pengguna_non_dpt_p')
+                        )
                 elif response.status_code == 404:
                     print("TPS NOT FOUND")
                     break
@@ -142,69 +141,76 @@ def crawling_kpu(province_code):
         for tps in range(1, 1000):
             tps = str(tps).zfill(3)
             print(f"Provinsi: {provinsi}, Kota: {kota}, Kecamatan: {kecamatan}, Kelurahan: {kelurahan}, TPS: {tps}")
-            response = insert_data(provinsi, kota, kecamatan, kelurahan, tps)
-            if response.status_code == 404:
-                print("TPS NOT FOUND")
-                break
+            response, error = insert_data(provinsi, kota, kecamatan, kelurahan, tps)
+            if response:
+                if response.status_code == 404:
+                    print("TPS NOT FOUND")
+                    break
+                else:
+                    print("Failed to get data from URL")
             else:
-                print("Failed to get data from URL")
+                print(error)
 
 
 def insert_data(provinsi=31, kota=3175, kecamatan=317505, kelurahan=3175051004, tps="073"):
-    url = f"https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/{provinsi}/{kota}/{kecamatan}/{kelurahan}/{kelurahan}{tps}.json"
-    payload = {}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
+    try:
+        url = f"https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/{provinsi}/{kota}/{kecamatan}/{kelurahan}/{kelurahan}{tps}.json"
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-    if response.status_code == 200:
-        data = response.json()
-        datatps, created = Tps.objects.get_or_create(
-            name=f"{kelurahan}{tps}",
-            psu=data['psu'],
-            ts=data['ts'],
-            status_suara=data['status_suara'],
-            status_adm=data['status_adm'],
-            url=f"https://pemilu2024.kpu.go.id/pilpres/hitung-suara/{provinsi}/{kota}/{kecamatan}/{kelurahan}/{kelurahan}{tps}",
-        )
+        if response.status_code == 200:
+            data = response.json()
+            datatps, created = Tps.objects.get_or_create(
+                name=f"{kelurahan}{tps}",
+                psu=data['psu'],
+                ts=data['ts'],
+                status_suara=data['status_suara'],
+                status_adm=data['status_adm'],
+                url=f"https://pemilu2024.kpu.go.id/pilpres/hitung-suara/{provinsi}/{kota}/{kecamatan}/{kelurahan}/{kelurahan}{tps}",
+            )
 
-        print(f"TPS: {datatps}, Created: {created}")
+            print(f"TPS: {datatps}, Created: {created}")
 
-        data_chart = data['chart']
-        if data_chart:
-            for key, value in data_chart.items():
-                Chart.objects.get_or_create(
+            data_chart = data['chart']
+            if data_chart:
+                for key, value in data_chart.items():
+                    Chart.objects.get_or_create(
+                        tps=datatps,
+                        name=key,
+                        count=value
+                    )
+
+            data_image = data['images']
+            for image in data_image:
+                Image.objects.get_or_create(
                     tps=datatps,
-                    name=key,
-                    count=value
+                    url=image
                 )
 
-        data_image = data['images']
-        for image in data_image:
-            Image.objects.get_or_create(
-                tps=datatps,
-                url=image
-            )
+            if data.get("administrasi"):
+                value = data.get("administrasi")
+                Administration.objects.get_or_create(
+                    tps=datatps,
+                    suara_sah=value.get('suara_sah'),
+                    suara_total=value.get('suara_total'),
+                    pemilih_dpt_l=value.get('pemilih_dpt_l'),
+                    pemilih_dpt_p=value.get('pemilih_dpt_p'),
+                    pengguna_dpt_j=value.get('pengguna_dpt_j'),
+                    pengguna_dpt_l=value.get('pengguna_dpt_l'),
+                    pengguna_dpt_p=value.get('pengguna_dpt_p'),
+                    pengguna_dptb_j=value.get('pengguna_dptb_j'),
+                    pengguna_dptb_l=value.get('pengguna_dptb_l'),
+                    pengguna_dptb_p=value.get('pengguna_dptb_p'),
+                    suara_tidak_sah=value.get('suara_tidak_sah'),
+                    pengguna_total_j=value.get('pengguna_total_j'),
+                    pengguna_total_l=value.get('pengguna_total_l'),
+                    pengguna_total_p=value.get('pengguna_total_p'),
+                    pengguna_non_dpt_j=value.get('pengguna_non_dpt_j'),
+                    pengguna_non_dpt_l=value.get('pengguna_non_dpt_l'),
+                    pengguna_non_dpt_p=value.get('pengguna_non_dpt_p')
+                )
+        return response, None
+    except Exception as e:
+        return None, e
 
-        if data.get("administrasi"):
-            value = data.get("administrasi")
-            Administration.objects.get_or_create(
-                tps=datatps,
-                suara_sah=value.get('suara_sah'),
-                suara_total=value.get('suara_total'),
-                pemilih_dpt_l=value.get('pemilih_dpt_l'),
-                pemilih_dpt_p=value.get('pemilih_dpt_p'),
-                pengguna_dpt_j=value.get('pengguna_dpt_j'),
-                pengguna_dpt_l=value.get('pengguna_dpt_l'),
-                pengguna_dpt_p=value.get('pengguna_dpt_p'),
-                pengguna_dptb_j=value.get('pengguna_dptb_j'),
-                pengguna_dptb_l=value.get('pengguna_dptb_l'),
-                pengguna_dptb_p=value.get('pengguna_dptb_p'),
-                suara_tidak_sah=value.get('suara_tidak_sah'),
-                pengguna_total_j=value.get('pengguna_total_j'),
-                pengguna_total_l=value.get('pengguna_total_l'),
-                pengguna_total_p=value.get('pengguna_total_p'),
-                pengguna_non_dpt_j=value.get('pengguna_non_dpt_j'),
-                pengguna_non_dpt_l=value.get('pengguna_non_dpt_l'),
-                pengguna_non_dpt_p=value.get('pengguna_non_dpt_p')
-            )
-    return response
