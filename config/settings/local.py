9bac0451e74f785653e5c80d64bdad9a4209c5cd
Abdu <abdu@karadigital.co>
2024-02-16 23:16:35 +0700

    oke gas oke gas

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "upright-infinite-shiner.ngrok-free.app", "jagapemilu.mufakat.id",
-                 "http://217.76.58.132:8000/"]
+                 "217.76.58.132"]
 
 # CACHES
 # ------------------------------------------------------------------------------
