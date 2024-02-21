cc7beb63f9e537089c42ab823c43f8a1f4a6d1d9
Abdu <47818989+muttayoshi@users.noreply.github.com>
2024-02-21 13:44:15 +0700

    Update admin.py

--- pemilu/locations/admin.py
+++ pemilu/locations/admin.py
@@ -31,29 +31,29 @@ class KelurahanAdmin(admin.ModelAdmin):
     list_filter = ("created", "modified")
 
 
-@admin.register(TingkatSatu)
-class TingkatSatuAdmin(admin.ModelAdmin):
-    list_display = ("name", "code", "created", "modified")
-    search_fields = ("name", "code")
-    list_filter = ("created", "modified")
+# @admin.register(TingkatSatu)
+# class TingkatSatuAdmin(admin.ModelAdmin):
+#     list_display = ("name", "code", "created", "modified")
+#     search_fields = ("name", "code")
+#     list_filter = ("created", "modified")
 
 
-@admin.register(TingkatDua)
-class TingkatDuaAdmin(admin.ModelAdmin):
-    list_display = ("name", "code", "tingkat_satu", "created", "modified")
-    search_fields = ("name", "code", "tingkat_satu")
-    list_filter = ("created", "modified")
+# @admin.register(TingkatDua)
+# class TingkatDuaAdmin(admin.ModelAdmin):
+#     list_display = ("name", "code", "tingkat_satu", "created", "modified")
+#     search_fields = ("name", "code", "tingkat_satu")
+#     list_filter = ("created", "modified")
 
 
-@admin.register(TingkatTiga)
-class TingkatTigaAdmin(admin.ModelAdmin):
-    list_display = ("name", "code", "tingkat_dua", "created", "modified")
-    search_fields = ("name", "code", "tingkat_dua")
-    list_filter = ("created", "modified")
+# @admin.register(TingkatTiga)
+# class TingkatTigaAdmin(admin.ModelAdmin):
+#     list_display = ("name", "code", "tingkat_dua", "created", "modified")
+#     search_fields = ("name", "code", "tingkat_dua")
+#     list_filter = ("created", "modified")
 
 
-@admin.register(TingkatEmpat)
-class TingkatEmpatAdmin(admin.ModelAdmin):
-    list_display = ("name", "code", "tingkat_tiga", "created", "modified")
-    search_fields = ("name", "code", "tingkat_tiga")
-    list_filter = ("created", "modified")
+# @admin.register(TingkatEmpat)
+# class TingkatEmpatAdmin(admin.ModelAdmin):
+#     list_display = ("name", "code", "tingkat_tiga", "created", "modified")
+#     search_fields = ("name", "code", "tingkat_tiga")
+#     list_filter = ("created", "modified")
