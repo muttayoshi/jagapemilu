306799bcc48deda0bc3a707d270daf4e5d264f42
Abdu <47818989+muttayoshi@users.noreply.github.com>
2024-02-29 09:49:31 +0700

    Update tasks.py

--- pemilu/locations/tasks.py
+++ pemilu/locations/tasks.py
@@ -2,7 +2,7 @@ from config import celery_app
 from pemilu.locations.utils import update_data_province
 
 
-@celery_app.task()
+@celery_app.task(soft_time_limit=60 * 60 * 24, time_limit=60 * 60 * 24)
 def update_province():
     update_data_province()
     return "update_province"
